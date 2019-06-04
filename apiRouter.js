bde6d0de7e61dd00e59975f3118fc24a247d141a
Kevin Boucherit <boucherit.kevin@gmail.com>
2019-06-04 16:21:05 +0200

    SOON

---
+++ apiRouter.js
@@ -0,0 +1,26 @@
+
+// Imports
+var express = require('express');
+var usersCtrl = require('./routes/usersCtrl');
+var messagesCtrl = require('./routes/messagesCtrl');
+
+
+// Router
+exports.router = (function () {
+  var apiRouter = express.Router();
+
+  // Users routes
+  apiRouter.route('/users/register/').post(usersCtrl.register);
+  apiRouter.route('/users/login/').post(usersCtrl.login);
+  apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
+  apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+  // Messages routes
+  apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+  apiRouter.route('/messages/').get(messagesCtrl.listMessages);
+
+  return apiRouter
+})()
+
+
+
---
+++ models/message.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Message = sequelize.define('Message', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    attachment: DataTypes.STRING,
+    likes: DataTypes.INTEGER
+  }, {
+      classMethods: {
+        associate: function (models) {
+          // associations can be defined here
+
+          models.Message.belongsTo(models.User, {
+            foreignKey: {
+              allowNull: false
+            }
+          })
+        }
+      }
+    });
+  return Message;
+}; 
